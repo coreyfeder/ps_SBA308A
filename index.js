8c5852b8497eaeb461ab87eb44851dd82e5d64de
Corey Feder <corey.feder@gmail.com>
2024-03-09 02:26:20 -0500

    damn sleep damn it all to hell again

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ const api_port = ""
 const api_url = `${api_protocol}://${api_host}${(api_port) ? ":"+apiport : ""}/${api_endpoint}`
 
 const url = (page=pagenum || 1, per_page=images_per_page || 3) => {
-    
+
 }
 
-let page_num = 1
-let images_per_page = 3
+let page = 1
+let per_page = 3
 
 // -H "Authorization: $YOUR_API_KEY" 
 // "https://api.pexels.com/v1/collections/clyoggb?page=1&per-page=3" 
 // "https://api.pexels.com/v1/collections/clyoggb?page=1&per_page=3"
 
-
 "https://api.pexels.com/v1/search?query=ferret&page=1&per_page=3"
+/* 
+{protocol=https}
+://
+{host=api.pexels.com}
+/v1/collections/
+{collection=clyoggb}
+/?page=
+{page}
+&per_page=
+{per_page}
+ */
+
 
 let apikey = 'OmSbPGDLr7S0nM4ym7slxHFdTKRWamIvxVVZiQoZnDeier1JOc9n4M9y'
 
@@ -43,6 +54,13 @@ Weezils provided by <a href="https://www.pexels.com">Pexels</a>.
 
 GET, POST, PUT, PATCH
 
+response payload
 
+payload.keys = [ 'page', 'per_page', 'media', 'total_results', 'next_page', 'id' ]
 
-
+for (let k of Object.keys(payload)) { if (k != 'media') { console.log(`${k}: ${payload[k]}`) } }
+> page: 1
+> per_page: 3
+> total_results: 33
+> next_page: https://api.pexels.com/v1/collections/clyoggb/?page=2&per_page=3
+> id: clyoggb
